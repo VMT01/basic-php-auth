1e68a15f6175cde0b79aa17bdfdb999825f63810
VMT01 <vuminhthanh8501@gmail.com>
2025-04-14 14:03:07 +0700

    Add css for error page

--- src/views/_error.php
+++ src/views/_error.php
@@ -2,9 +2,16 @@
 
 <head>
     <title>Error</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://static-gcdn.basecdn.net/account/image/fav.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="https://static-gcdn.basecdn.net/account/image/fav.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:500,400,300,400italic,700,700italic,400italic,300italic&subset=vietnamese,latin">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/error.css">
 </head>
 
 <body>
-    <h3><?php echo $exception->getCode() ?> - <?php echo $exception->getMessage() ?></h3>
+    <h1><?php echo $exception->getCode() ?> - <?php echo $exception->getMessage() ?></h1>
     <button><a href="login">Back to login page</a></button>
 </body>
